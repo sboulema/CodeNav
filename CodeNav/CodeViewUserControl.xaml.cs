e297e026e15844bf5105e3c75f634047abc5cacb
Samir L. Boulema <sboulema@gmail.com>
2016-12-08 11:32:27 +0100

    Improve scrolling

--- CodeNav/CodeViewUserControl.xaml.cs
+++ CodeNav/CodeViewUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using CodeNav.Models;
 using EnvDTE;
 
@@ -21,6 +23,19 @@ namespace CodeNav
         {
             if (((ListBox) sender).SelectedItem == null) return;           
             (_dte.ActiveDocument.Selection as TextSelection).MoveToPoint(((CodeItem)((ListBox)sender).SelectedItem).StartPoint);
+        }
+
+        private void UIElement_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
         }
     }
 }
