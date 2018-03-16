2fb1d0fdf479951822acd9a877711d7e2db6af59
Samir L. Boulema <sboulema@gmail.com>
2018-03-16 16:46:57 +0100

    Add unit tests for sorting

---
+++ CodeNav.Tests/Files/TestSorting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeNav.Tests.Files
+{
+    class TestSorting
+    {
+        void C()
+        {
+
+        }
+
+        void B()
+        {
+
+        }
+
+        void A()
+        {
+
+        }
+    }
+}
---
+++ CodeNav.Tests/HelperTests/SortHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeNav.Helpers;
+using CodeNav.Mappers;
+using CodeNav.Models;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CodeNav.Tests.HelperTests
+{
+    [TestFixture]
+    public class SortHelperTests
+    {
+        [TestCase(SortOrderEnum.SortByFile, new string[] { "C", "B", "A" })]
+        [TestCase(SortOrderEnum.SortByName, new string[] { "A", "B", "C" })]
+        public void ItemsSorting(SortOrderEnum sortOrder, string[] methodNames)
+        {
+            var document = SyntaxMapper.MapDocument(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\Files\\TestSorting.cs"));
+            var viewModel = new CodeDocumentViewModel { CodeDocument = document, SortOrder = sortOrder };
+
+            viewModel.CodeDocument = SortHelper.Sort(viewModel);
+
+            var sortingClass = (document.First() as IMembers).Members.First() as CodeClassItem;
+
+            Assert.AreEqual(methodNames[0], sortingClass.Members.First().Name);
+            Assert.AreEqual(methodNames[1], sortingClass.Members[1].Name);
+            Assert.AreEqual(methodNames[2], sortingClass.Members.Last().Name);
+
+            Assert.AreEqual(sortOrder, viewModel.SortOrder);
+        }
+    }
+}
