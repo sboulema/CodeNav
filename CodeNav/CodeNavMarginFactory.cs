8f97b76ca6bfb55c14d6941417b56974bfd4c363
Samir L. Boulema <sboulema@gmail.com>
2018-11-29 09:39:22 +0100

    Fix Defaulting to always collapsed #77

--- CodeNav/CodeNavMarginFactory.cs
+++ CodeNav/CodeNavMarginFactory.cs
@@ -8,14 +8,9 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 using CodeNav.Models;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.Text.Outlining;
 
 namespace CodeNav
 {
-    /// <summary>
-    /// Export a <see cref="IWpfTextViewMarginProvider"/>, which returns an instance of the margin for the editor to use.
-    /// </summary>
     [Export(typeof(IWpfTextViewMarginProvider))]
     [Name(CodeNavMargin.MarginName + "Left")]
     [Order(Before = PredefinedMarginNames.Left)]         // Ensure that the margin occurs left of the editor window
@@ -23,7 +18,7 @@ namespace CodeNav
     [ContentType("CSharp")]                             // Show this margin for all code-based types
     [ContentType("Basic")]
     [TextViewRole(PredefinedTextViewRoles.Debuggable)]  // This is to prevent the margin from loading in the diff view
-    internal sealed class CodeNavMarginFactory : IWpfTextViewMarginProvider
+    internal sealed class CodeNavLeftFactory : IWpfTextViewMarginProvider
     {
         [Import(typeof(SVsServiceProvider))]
         private IServiceProvider ServiceProvider { get; set; }
@@ -31,34 +26,12 @@ namespace CodeNav
         [Import(typeof(VisualStudioWorkspace))]
         private VisualStudioWorkspace Workspace { get; set; }
 
-        #region IWpfTextViewMarginProvider
-
-        /// <summary>
-        /// Creates an <see cref="IWpfTextViewMargin"/> for the given <see cref="IWpfTextViewHost"/>.
-        /// </summary>
-        /// <param name="wpfTextViewHost">The <see cref="IWpfTextViewHost"/> for which to create the <see cref="IWpfTextViewMargin"/>.</param>
-        /// <param name="marginContainer">The margin that will contain the newly-created margin.</param>
-        /// <returns>The <see cref="IWpfTextViewMargin"/>.
-        /// The value may be null if this <see cref="IWpfTextViewMarginProvider"/> does not participate for this context.
-        /// </returns>
         public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
         {
-            if (Settings.Default.MarginSide != MarginSideEnum.Left) return null;
-
-            var dte = (DTE)ServiceProvider.GetService(typeof(DTE));
-            var outliningManagerService = OutliningHelper.GetOutliningManagerService(ServiceProvider);
-
-            var codeNav = new CodeNavMargin(wpfTextViewHost, dte, outliningManagerService, Workspace, MarginSideEnum.Left);
-
-            return codeNav;
+            return CodeNavFactory.CreateMargin(wpfTextViewHost, Workspace, ServiceProvider, MarginSideEnum.Left);
         }
-
-        #endregion
     }
 
-    /// <summary>
-    /// Export a <see cref="IWpfTextViewMarginProvider"/>, which returns an instance of the margin for the editor to use.
-    /// </summary>
     [Export(typeof(IWpfTextViewMarginProvider))]
     [Name(CodeNavMargin.MarginName + "Right")]
     [Order(After = PredefinedMarginNames.RightControl)]  // Ensure that the margin occurs after the vertical scrollbar
@@ -74,28 +47,25 @@ namespace CodeNav
         [Import(typeof(VisualStudioWorkspace))]
         private VisualStudioWorkspace Workspace { get; set; }
 
-        #region IWpfTextViewMarginProvider
-
-        /// <summary>
-        /// Creates an <see cref="IWpfTextViewMargin"/> for the given <see cref="IWpfTextViewHost"/>.
-        /// </summary>
-        /// <param name="wpfTextViewHost">The <see cref="IWpfTextViewHost"/> for which to create the <see cref="IWpfTextViewMargin"/>.</param>
-        /// <param name="marginContainer">The margin that will contain the newly-created margin.</param>
-        /// <returns>The <see cref="IWpfTextViewMargin"/>.
-        /// The value may be null if this <see cref="IWpfTextViewMarginProvider"/> does not participate for this context.
-        /// </returns>
         public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
         {
-            if (Settings.Default.MarginSide != MarginSideEnum.Right) return null;
+            return CodeNavFactory.CreateMargin(wpfTextViewHost, Workspace, ServiceProvider, MarginSideEnum.Right);
+        }
+    }
 
-            var dte = (DTE)ServiceProvider.GetService(typeof(DTE));
-            var outliningManagerService = (IOutliningManagerService)ServiceProvider.GetService(typeof(IOutliningManagerService));
+    internal static class CodeNavFactory
+    {
+        public static IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, 
+            VisualStudioWorkspace visualStudioWorkspace, IServiceProvider serviceProvider, MarginSideEnum side)
+        {
+            if (Settings.Default.MarginSide != side) return null;
 
-            var codeNav = new CodeNavMargin(wpfTextViewHost, dte, outliningManagerService, Workspace, MarginSideEnum.Right);
+            var dte = (DTE)serviceProvider.GetService(typeof(DTE));
+            var outliningManagerService = OutliningHelper.GetOutliningManagerService(serviceProvider);
+
+            var codeNav = new CodeNavMargin(wpfTextViewHost, dte, outliningManagerService, visualStudioWorkspace, side);
 
             return codeNav;
         }
-
-        #endregion
-    }
+    } 
 }
