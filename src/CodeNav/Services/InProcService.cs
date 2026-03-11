fe8652c5b38f1c00f2caf16091f95c8a4694f71a
Samir Boulema <sboulema@gmail.com>
2026-03-11 20:59:33 +0100

    feat: Implement outlining

--- src/CodeNav/Services/InProcService.cs
+++ src/CodeNav/Services/InProcService.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.Extensibility.VSSdkCompatibility;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
 using Microsoft.VisualStudio.TextManager.Interop;
 
 namespace CodeNav.Services;
@@ -15,15 +16,18 @@ internal class InProcService : IInProcService
 {
     private readonly VisualStudioExtensibility _extensibility;
     private readonly MefInjection<IVsEditorAdaptersFactoryService> _editorAdaptersFactoryService;
+    private readonly MefInjection<IOutliningManagerService> _outliningManagerFactoryService;
     private readonly AsyncServiceProviderInjection<SVsTextManager, IVsTextManager> _textManager;
 
     public InProcService(
         VisualStudioExtensibility extensibility,
         MefInjection<IVsEditorAdaptersFactoryService> editorAdaptersFactoryService,
+        MefInjection<IOutliningManagerService> outliningManagerFactoryService,
         AsyncServiceProviderInjection<SVsTextManager, IVsTextManager> textManager)
     {
         _extensibility = extensibility;
         _editorAdaptersFactoryService = editorAdaptersFactoryService;
+        _outliningManagerFactoryService = outliningManagerFactoryService;
         _textManager = textManager;
     }
 
@@ -39,6 +43,75 @@ internal class InProcService : IInProcService
         await _extensibility.Shell().ShowPromptAsync("Hello from out-of-proc! (Showing this message from (in-proc)", PromptOptions.OK, cancellationToken);
     }
 
+    public async Task ExpandOutlineRegion(int start, int length)
+    {
+        try
+        {
+            // Not using context.GetActiveTextViewAsync here because VisualStudio.Extensibility doesn't support outlining yet.
+            var textView = await GetCurrentTextViewAsync();
+
+            var outliningManager = await GetOutliningManager(textView);
+
+            var outlineRegion = await GetOutlineRegionForSpan(textView, outliningManager, start, length);
+
+            // Check if the outline region is collapsed before expanding
+            if (outlineRegion?.IsCollapsed != true ||
+                outlineRegion is not ICollapsed collapsedOutlineRegion)
+            {
+                return;
+            }
+
+            outliningManager.Expand(collapsedOutlineRegion);
+        }
+        catch (Exception)
+        {
+            // TODO: Implement in-proc error logging
+        }
+    }
+
+    public async Task CollapseOutlineRegion(int start, int length)
+    {
+        try
+        {
+            // Not using context.GetActiveTextViewAsync here because VisualStudio.Extensibility doesn't support outlining yet.
+            var textView = await GetCurrentTextViewAsync();
+
+            var outliningManager = await GetOutliningManager(textView);
+
+            var outlineRegion = await GetOutlineRegionForSpan(textView, outliningManager, start, length);
+
+            outliningManager.TryCollapse(outlineRegion);
+        }
+        catch (Exception)
+        {
+            // TODO: Implement in-proc error logging
+        }
+    }
+
+    private async Task<ICollapsible?> GetOutlineRegionForSpan(
+        IWpfTextView textView, IOutliningManager outliningManager,
+        int start, int length)
+    {
+        // Get all outline regions for the given span
+        var span = new SnapshotSpan(textView.TextSnapshot, start, length);
+
+        var outlineRegions = outliningManager.GetAllRegions(span);
+
+        // Get the first outline region that has the same span start
+        return outlineRegions.FirstOrDefault(outlineRegion => GetSpan(outlineRegion).Start == start);
+    }
+
+    private SnapshotSpan GetSpan(ICollapsible outlineRegion)
+        => outlineRegion.Extent.GetSpan(outlineRegion.Extent.TextBuffer.CurrentSnapshot);
+
+    private async Task<IOutliningManager> GetOutliningManager(IWpfTextView textView)
+    {
+        var outliningManagerService = await _outliningManagerFactoryService.GetServiceAsync();
+        var outliningManager = outliningManagerService.GetOutliningManager(textView);
+
+        return outliningManager;
+    }
+
     public async Task TextViewScrollToSpan(int start, int length)
     {
         try
@@ -46,7 +119,7 @@ internal class InProcService : IInProcService
             // Not using context.GetActiveTextViewAsync here because VisualStudio.Extensibility doesn't support viewscroller yet.
             var textView = await GetCurrentTextViewAsync();
 
-            var span = new SnapshotSpan(textView.TextSnapshot, new Span(start, length));
+            var span = new SnapshotSpan(textView.TextSnapshot, start, length);
 
             // Switch to the UI thread to ensure we can interact with the view scroller.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
@@ -61,7 +134,7 @@ internal class InProcService : IInProcService
 
     private async Task<IWpfTextView> GetCurrentTextViewAsync()
     {
-        IVsEditorAdaptersFactoryService editorAdapter = await _editorAdaptersFactoryService.GetServiceAsync();
+        var editorAdapter = await _editorAdaptersFactoryService.GetServiceAsync();
         var view = editorAdapter.GetWpfTextView(await GetCurrentNativeTextViewAsync());
         Assumes.Present(view);
         return view;
